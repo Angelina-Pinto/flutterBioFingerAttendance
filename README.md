bfdbbe275f7e630f725deae98187ce4113d3c0ac
angelina.pg <angelinapinto1278@gmail.com>
2024-07-16 21:54:32 -0600

    primer coomit

---
+++ README.md
@@ -0,0 +1,3 @@
+# admin_bio_attendance
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/admin_bio_attendance/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.admin_bio_attendance
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
